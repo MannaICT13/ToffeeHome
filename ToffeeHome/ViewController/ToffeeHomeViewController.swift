9d6ee8ffcbab0312501a26d5e1da66788ad88073
Md Khaled Hasan <khaled.hasan@nexdecade.com>
2023-08-07 14:03:23 +0600

    create FSPager View for image sliding

--- ToffeeHome/ViewController/ToffeeHomeViewController.swift
+++ ToffeeHome/ViewController/ToffeeHomeViewController.swift
@@ -8,10 +8,77 @@
 import UIKit
 
 class ToffeeHomeViewController: UIViewController {
-
+    
+    enum Section {
+        case pagerView
+    }
+    
+    struct Item: Hashable {
+        let title: String?
+        private let identifier = UUID()
+    }
+    
+    private var collectionView: UICollectionView! = nil
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Item>
+    
+    private var dataSource: DataSource! = nil
+    private var snapshot = Snapshot()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupCollectionView()
+        configureCompositionalLayout()
+        configurediffableDataSource()
     }
 }
 
+extension ToffeeHomeViewController {
+    private func createCollectionLayoutSection() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(193.0)), subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout.init())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        
+        let nib = UINib(nibName: PagerCollectionViewCell.reuseIdentifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: PagerCollectionViewCell.reuseIdentifier)
+    }
+    
+    private func configureCompositionalLayout(){
+        let layout = UICollectionViewCompositionalLayout {sectionIndex,enviroment in
+            switch sectionIndex {
+            case  0:
+                return self.createCollectionLayoutSection()
+            default:
+                return self.createCollectionLayoutSection()
+            }
+        }
+        collectionView.setCollectionViewLayout(layout, animated: false)
+    }
+    
+    private func configurediffableDataSource() {
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) in
+            guard indexPath.section < self.dataSource.snapshot().numberOfSections else { fatalError("Invalid section index") }
+            
+            switch self.dataSource.snapshot().sectionIdentifiers[indexPath.section] {
+            case .pagerView:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PagerCollectionViewCell.reuseIdentifier, for: indexPath) as! PagerCollectionViewCell
+                return cell
+            }
+        })
+        
+        var snapShot = Snapshot()
+        snapShot.appendSections([.pagerView])
+        snapShot.appendItems([Item(title: "")], toSection: .pagerView)
+        dataSource.apply(snapShot, animatingDifferences: false)
+    }
+}
---
+++ ToffeeHome/Views/PagerCollectionViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  PagerCollectionViewCell.swift
+//  ToffeeHome
+//
+//  Created by Khaled on 7/8/23.
+//
+
+import UIKit
+
+class PagerCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "PagerCollectionViewCell"
+    fileprivate var numberOfItems = 7
+    fileprivate let imageNames = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg"]
+    
+    @IBOutlet private weak var pagerView: FSPagerView! {
+        didSet {
+            self.pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+            self.pagerView.itemSize = FSPagerView.automaticSize
+            self.pagerView.automaticSlidingInterval = 3.0 - self.pagerView.automaticSlidingInterval
+        }
+    }
+    
+    @IBOutlet weak var pageControl: FSPageControl!{
+        didSet {
+            self.pageControl.numberOfPages = self.imageNames.count
+            self.pageControl.contentHorizontalAlignment = .right
+            self.pageControl.contentInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        pagerView.delegate = self
+        pagerView.dataSource = self
+    }
+}
+
+extension PagerCollectionViewCell: FSPagerViewDataSource,FSPagerViewDelegate  {
+    public func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return self.numberOfItems
+    }
+    
+    public func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
+        cell.imageView?.image = UIImage(named: self.imageNames[index])
+        cell.imageView?.contentMode = .scaleAspectFill
+        cell.imageView?.clipsToBounds = true
+        return cell
+    }
+    
+    // MARK:- FSPagerView Delegate
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        pagerView.deselectItem(at: index, animated: true)
+        pagerView.scrollToItem(at: index, animated: true)
+    }
+    
+    func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
+        self.pageControl.currentPage = targetIndex
+    }
+    
+    func pagerViewDidEndScrollAnimation(_ pagerView: FSPagerView) {
+        self.pageControl.currentPage = pagerView.currentIndex
+    }
+
+}
