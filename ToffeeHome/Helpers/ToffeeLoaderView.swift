a9005c196c90129e985b85feb328e37625837a2e
Md Khaled Hasan <khaled.hasan@nexdecade.com>
2023-08-17 12:16:38 +0600

    remove public init

--- ToffeeHome/Helpers/ToffeeLoaderView.swift
+++ ToffeeHome/Helpers/ToffeeLoaderView.swift
@@ -9,8 +9,6 @@ import Foundation
 import UIKit
 
 class ToffeeLoader {
-    public init() {}
-    
     func startShining(_ view: UIView) {
         view.layoutIfNeeded()
         animate(view: view, start: true)
